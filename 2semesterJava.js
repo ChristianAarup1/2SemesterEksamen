99f53dd067b42bd7125ee3ece3431a2b6cdd6cc6
Alexander Bartram Laursen <alexander@vildmedtog.dk>
2023-05-31 14:03:50 +0200

    Update 2semesterJava.js

--- 2semesterJava.js
+++ 2semesterJava.js
@@ -1,1 +1,31 @@
-// JavaScript Document
+// JavaScript Documentfunction imgNext() {
+
+      var image = document.getElementById("img1")
+if (image.src.match("SlidePictures/image.png")) {
+
+    image.src = "SlidePictures/image2.png";
+} else if (image.src.match("SlidePictures/image2.png")) {
+    image.src = "SlidePictures/image3.png";
+
+} else if (image.src.match("SlidePictures/image3.png")) {
+    image.src = "SlidePictures/image4.png";
+} else {
+    image.src = "SlidePictures/image.png";
+}
+}
+
+function imgPrev() {
+
+      var image = document.getElementById("img1")
+if (image.src.match("SlidePictures/image.png")) {
+     image.src = "SlidePictures/image4.png";
+
+} else if (image.src.match("SlidePictures/image4.png")) {
+    image.src = "SlidePictures/image3.png";
+
+} else if (image.src.match("SlidePictures/image3.png")) {
+    image.src = "SlidePictures/image2.png";
+} else {
+    image.src = "SlidePictures/image.png";
+}
+}
