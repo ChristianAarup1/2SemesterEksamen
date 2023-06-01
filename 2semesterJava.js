90be3ea565c487ca7cd70564f28fe673e70b8087
Alexander Bartram Laursen <alexander@vildmedtog.dk>
2023-06-02 00:40:11 +0200

    Update 2semesterJava.js

--- 2semesterJava.js
+++ 2semesterJava.js
@@ -2,6 +2,8 @@
 
 //Define HTML/CSS
 
+
+
 function imgNext() {
 
       var image = document.getElementById("img1")
@@ -11,9 +13,15 @@ if (image.src.match("Billeder/Booth.jpg")) {
 } else if (image.src.match("Billeder/Boba_Revan.jpg")) {
     image.src = "Billeder/Presentation.jpg";
 } else {
-    image.src = "Billeder/Booth.jpg";
+    // Replace the image with the YouTube video embed
+    var videoContainer = document.createElement("div");
+    videoContainer.innerHTML = '<iframe width="1120" height="630" src="https://www.youtube.com/embed/5PhVUPImx1s" title="KoyoCon 2022 Cosplay Highlights - Cosplay Denmark" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>';
+
+	 image.parentNode.replaceChild(videoContainer, image);
 }
 }
+
+
 
 function imgPrev() {
 
